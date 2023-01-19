1ebe602fcc746f206910ba75ef74af33b933e299
EduardsLeonovics <108423884+EduardsLeonovics@users.noreply.github.com>
2023-01-19 17:30:01 +0200

    Add files via upload
    Updated whole resume page with Abot me / Projects section as well as changed color scheme

---
+++ index.js
@@ -0,0 +1,12 @@
+let resumeButton = document.querySelector('.resume-button');
+let projectsButton = document.querySelector('.projects-button');
+
+resumeButton.addEventListener('mouseover', function() {
+   this.style.backgroundColor = '#85BEDA';
+   this.style.transition = 'background-color 0.3s ease-in';
+});
+
+projectsButton.addEventListener('mouseover', function() {
+   this.style.backgroundColor = '#85BEDA';
+   this.style.transition = 'background-color 0.3s ease-in';
+});
